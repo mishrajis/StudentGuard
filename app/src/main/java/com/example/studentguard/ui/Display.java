6e5d5751dc2e36556362d5b6bc6a8305c0d60f37
mishrajis <sm741739@gmail.com>
2022-05-22 12:53:14 +0530

    kuchbhi

---
+++ app/src/main/java/com/example/studentguard/ui/Display.java
@@ -0,0 +1,38 @@
+package com.example.studentguard.ui;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.studentguard.R;
+
+public class Display extends Fragment {
+
+    private DisplayViewModel mViewModel;
+
+    public static Display newInstance() {
+        return new Display();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_display, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(DisplayViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/example/studentguard/ui/DisplayViewModel.java
@@ -0,0 +1,7 @@
+package com.example.studentguard.ui;
+
+import androidx.lifecycle.ViewModel;
+
+public class DisplayViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
